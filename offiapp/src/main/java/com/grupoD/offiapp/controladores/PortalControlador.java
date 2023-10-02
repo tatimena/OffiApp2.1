d94f64b28dd1218a43f3f38b5059ed59d04b1065
Leonardo Alvez <95445833+leondalvez@users.noreply.github.com>
2023-10-02 20:19:08 -0300

    Index revisado

--- offiapp/src/main/java/com/grupoD/offiapp/controladores/PortalControlador.java
+++ offiapp/src/main/java/com/grupoD/offiapp/controladores/PortalControlador.java
@@ -1,7 +1,7 @@
 package com.grupoD.offiapp.controladores;
 
-
 import com.grupoD.offiapp.Entidades.Calificacion;
+import com.grupoD.offiapp.Entidades.Usuario;
 import com.grupoD.offiapp.excepciones.MiException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -35,12 +35,11 @@ public class PortalControlador {
         return "registro_usuario.html";
     }
 
-    
     @PostMapping("/registrar")
     public String Registrar(@RequestParam MultipartFile archivo, @RequestParam String nombreUser, @RequestParam String direccion, @RequestParam String email, @RequestParam String password, String password2, Integer telefono, String servicio, Integer precioHora, String descripcion, ModelMap modelo) throws MiException {
 
         try {
-            usuarioServicio.registrar(archivo,nombreUser, direccion, email, password, password2, telefono, servicio, precioHora, descripcion);
+            usuarioServicio.registrar(archivo, nombreUser, direccion, email, password, password2, telefono, servicio, precioHora, descripcion);
             modelo.put("exito", "Usted se ha registrado correctamente");
             return "index.html";
         } catch (MiException ex) {
@@ -53,8 +52,6 @@ public class PortalControlador {
 
     }
 
-    
-
     @GetMapping("/login")
     public String login(@RequestParam(required = false) String error, ModelMap modelo) {
         if (error != null) {
@@ -66,14 +63,14 @@ public class PortalControlador {
     @GetMapping("/logueado")
     public String Logueado() {
 
-        return "usuariolog.html";
+        return "inicio1_usuario.html";
     }
-    
+
     @GetMapping("/conocenos")
-    public String conocenos(){
+    public String conocenos() {
         return "about_us.html";
     }
-    
+
     @GetMapping("/registroProveedor")
     public String RegistroProveedor() {
         return "registro_proveedor.html";
@@ -81,8 +78,8 @@ public class PortalControlador {
 
     @PostMapping("/registrarProveedor")
 
-    public String RegistrarProveedor(@RequestParam MultipartFile archivo, @RequestParam String nombreProv, 
-            @RequestParam String email,@RequestParam String direccion,
+    public String RegistrarProveedor(@RequestParam MultipartFile archivo, @RequestParam String nombreProv,
+            @RequestParam String email, @RequestParam String direccion,
             @RequestParam String password, @RequestParam String password2, @RequestParam Integer telefono, @RequestParam String servicio,
             @RequestParam Integer precioHora, @RequestParam(required = false) String descripcion, ModelMap modelo) {
 
@@ -98,27 +95,38 @@ public class PortalControlador {
             return "registro_proveedor.html";
         }
     }
-     @GetMapping("/perfilProveedor")
+//     @GetMapping("/perfilProveedor")
+//    public String mostrarPerfilProveedor(Model model) {
+//        // Aquí puedes agregar lógica para obtener los datos del proveedor
+//        // y pasarlos al modelo para que se muestren en la vista
+//        model.addAttribute("nombreProveedor", "Nombre del Proveedor");
+//        model.addAttribute("descripcion", "Descripción del trabajo: Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+//        model.addAttribute("calificaciones", obtenerCalificacionesProveedor()); // Reemplaza con tu propia lógica
+//
+//        return "vistaPerfil.html"; // Este es el nombre de la vista HTML (perfilProveedor.html)
+//    }
+
+    @GetMapping("/perfilProveedor")
     public String mostrarPerfilProveedor(Model model) {
         // Aquí puedes agregar lógica para obtener los datos del proveedor
         // y pasarlos al modelo para que se muestren en la vista
-        model.addAttribute("nombreProveedor", "Nombre del Proveedor");
-        model.addAttribute("descripcion", "Descripción del trabajo: Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
-        model.addAttribute("calificaciones", obtenerCalificacionesProveedor()); // Reemplaza con tu propia lógica
+        Usuario proveedor = usuarioServicio.getOne("4e5c6689-4107-470c-9fac-50161cd30b15"); // Reemplaza "ID_DEL_PROVEEDOR" por el ID del proveedor que deseas mostrar
+        model.addAttribute("proveedor", proveedor);
+        
 
-        return "vistaPerfil.html"; // Este es el nombre de la vista HTML (perfilProveedor.html)
+        return "perfil_Proveedor.html"; // Nombre de la vista HTML que mostrará el perfil del proveedor
     }
 
     // Método de ejemplo para obtener calificaciones (reemplaza con tu propia lógica)
     private List<Calificacion> obtenerCalificacionesProveedor() {
         List<Calificacion> calificaciones = new ArrayList<>();
-       // calificaciones.add(new Calificacion(5, "01/01/2023", "Excelente proveedor. Muy satisfecho con su trabajo."));
+        // calificaciones.add(new Calificacion(5, "01/01/2023", "Excelente proveedor. Muy satisfecho con su trabajo."));
         // Agrega más calificaciones
         return calificaciones;
     }
 }
 
-    /*
+/*
     @GetMapping("/proveedor/registro")
     public String RegistroProv() {
         return "registro_proveedor.html";
@@ -139,5 +147,4 @@ public class PortalControlador {
         }
 
     }
-*/
-
+ */
